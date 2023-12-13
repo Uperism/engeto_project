218e6937e9661d535d1b2288730c8d848b561e18
Daniel K <139114609+Uperism@users.noreply.github.com>
2023-12-13 13:40:09 +0100

    Update první otázky do finální podoby

--- 1) first question.sql
+++ 1) first question.sql
@@ -1,13 +1,14 @@
+-- Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 SELECT 
 	dksp.payroll_year AS year,
 	cpib.name AS industry_name,
-	MAX(dksp.salary) AS current_year_salary,
-	ROUND(AVG(dksp.salary), 0) AS average_salary,
-	LAG(MAX(dksp.salary)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) AS previous_year_salary,
-	ROUND(100 * (MAX(dksp.salary) - LAG(MAX(dksp.salary)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year)) / LAG(MAX(dksp.salary)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year), 2) AS previous_year_percent_change,
+	MAX(dksp.payroll_value) AS current_year_salary,
+	ROUND(AVG(dksp.payroll_value), 0) AS average_salary,
+	LAG(MAX(dksp.payroll_value)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) AS previous_year_salary,
+	ROUND(100 * (MAX(dksp.payroll_value) - LAG(MAX(dksp.payroll_value)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year)) / LAG(MAX(dksp.payroll_value)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year), 2) AS previous_year_percent_change,
 	CASE
-		WHEN MAX(dksp.salary) > LAG(MAX(dksp.salary)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) THEN 'Increase'
-		WHEN MAX(dksp.salary) < LAG(MAX(dksp.salary)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) THEN 'Decrease'
+		WHEN MAX(dksp.payroll_value) > LAG(MAX(dksp.payroll_value)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) THEN 'Increase'
+		WHEN MAX(dksp.payroll_value) < LAG(MAX(dksp.payroll_value)) OVER (PARTITION BY cpib.name ORDER BY dksp.payroll_year) THEN 'Decrease'
 		ELSE 'Stable - no change'
 	END AS change_type
 FROM t_daniel_kadar_project_SQL_primary_final dksp
@@ -17,40 +18,3 @@ WHERE dksp.industry_branch_code IS NOT NULL
 	AND dksp.unit_code = '200'
 	AND dksp.value_type_code = '5958'
 GROUP BY cpib.name, dksp.payroll_year;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
