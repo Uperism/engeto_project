16ed8e64c7b8b728d125317fc1b4f3f9cdb9b398
Daniel K <139114609+Uperism@users.noreply.github.com>
2023-12-13 13:36:15 +0100

    ○ Vyhotovení druhé tabulky

--- 5) fifth question.sql
+++ 5) fifth question.sql
@@ -1,1 +1,15 @@
-
+CREATE OR REPLACE TABLE t_daniel_kadar_project_SQL_secondary_final AS
+SELECT 
+	c.continent,
+	c.country,
+	e.GDP,
+	e.gini,
+	e.population,
+	e.`year`
+FROM countries c 
+LEFT JOIN economies e 
+	ON e.country = c.country 
+WHERE e.year >= 2000 
+	AND e.year <= 2021
+	AND c.continent = 'Europe'
+ORDER BY e.year;
