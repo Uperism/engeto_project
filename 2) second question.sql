b141e12444fc204348bed27e5caf274095e44bac
Daniel K <139114609+Uperism@users.noreply.github.com>
2023-12-13 13:47:39 +0100

    Update druhé otázky do finální podoby.

--- 2) second question.sql
+++ 2) second question.sql
@@ -2,16 +2,16 @@
 
 SELECT 
 	dksp.payroll_year AS year,
-	round(avg(dksp.salary), 0) AS average_earnings,
-	cpc.name AS name_of_item, 
-	round(dksp.price, 0) AS average_price_CZK,
-	round(avg(dksp.salary)/dksp.price, 0) AS amount,
-	cpc.price_unit AS units 
-FROM t_daniel_kadar_project_SQL_primary_final dksp
-LEFT JOIN czechia_price_category cpc
-	ON cpc.code = dksp.item_code
+	round(avg(dksp.payroll_value),0) AS average_salary,
+	cpc.name, 
+	round (dksp.average_value, 0) AS average_price_CZK,
+	round(avg(dksp.payroll_value)/dksp.average_value,0) AS amount,
+	cpc.price_unit
+FROM t_daniel_kadar_project_sql_primary_final dksp
+LEFT JOIN czechia_price_category cpc 
+	ON cpc.code = dksp.category_code 
 WHERE dksp.value_type_code = 5958
-	AND dksp.payroll_year IN (2006, 2018)
-	AND dksp.item_code IN (114201, 111301)
 	AND dksp.calculation_code = 200
- GROUP BY dksp.payroll_year, dksp.item_code;
+	AND dksp.category_code IN (114201, 111301)
+	AND dksp.payroll_year  IN (2006, 2018)
+GROUP BY dksp.payroll_year, dksp.category_code;
